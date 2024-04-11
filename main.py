e4a7ba3152ddf3d3617e4649384253065938c62f
Sara Hau <s.hau@mci4me.at>
2024-04-11 16:06:02 +0200

    Fertig

--- main.py
+++ main.py
@@ -2,24 +2,22 @@ from load_data import load_data
 import sort
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
 
 data = load_data('activity.csv')
 power_W = data['PowerOriginal']
 print(power_W)
 
-
-# Daten sortieren
-
-#sorted_data = sort.bubble_sort()
 sorted_data = sort.bubble_sort(power_W)
-# Grafik erstellen und speichern
 
+img_new = sorted_data[::-1]
 
-#plt.plot(sorted_data)
-img_new = np.rot90(sorted_data, 2)
-plt.plot(img_new)
+time_s = np.array(list(range(0,len(img_new))))/60
 
+plt.plot(time_s, img_new)
+plt.xlabel("Time/min")
+plt.ylabel("W/kg")
+
+plt.show()
 plt.savefig("figures/power_curve.png")
 
 
--- sort.py
+++ sort.py
@@ -1,10 +1,9 @@
-# sort.py
 
 def bubble_sort(data):
     n = len(data)
     for i in range(n-1):
         for j in range(0, n-i-1):
             if data[j] > data[j+1]:
-                data[j], data[j+1] = data[j+1], data[j]  # Tausche Elemente
+                data[j], data[j+1] = data[j+1], data[j] 
     return data
 
